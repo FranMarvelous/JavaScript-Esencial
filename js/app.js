e8ab5003e2620bd1f0d492661afc9d9ce552a23e
FranMarvelous <franmarvelous@gmail,com>
2023-05-10 20:31:33 -0400

    strict mode

--- js/app.js
+++ js/app.js
@@ -1,4 +1,11 @@
+"use strict"
+// modo estricto, me pide declarar funciones primero
+
 //alert("Hola desde un archivo externo");
 
 // este es un mensaje en consola
 console.log("Hola consola");
+
+//declaración de variable
+var x;
+x = "3.1416"
