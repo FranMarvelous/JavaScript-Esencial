cc4660a45d6551e99edb8058dd2436316ec19544
FranMarvelous <franmarvelous@gmail,com>
2023-05-10 20:50:04 -0400

    variables

---
+++ js/app.js
@@ -0,0 +1,29 @@
+"use strict"
+// modo estricto, me pide declarar funciones primero
+
+//alert("Hola desde un archivo externo");
+
+// este es un mensaje en consola
+console.log("Hola consola");
+
+//declaración de variable
+var x;
+x = "3.1416"
+
+//VAR globales y locales
+
+var nombre = "Fran";
+
+console.log(nombre)
+
+//Variables globales: puedo acceder a ella en cualquier parte
+//Var son destructivas, se sobreescriben.
+
+//función básica modificacion de variable solo dentro de funcion:
+
+function saludo (){
+    var nombre = "Rebe";
+        console.log(nombre)
+}
+
+return saludo();
